b7a54d35910abbee7dcb808ea91a3ac76aa9dbb0
Bryan Brito <bryanbrito@Bryans-MacBook-Air.local>
2023-05-31 22:33:20 -0300

    Suite de teste /task

---
+++ cypress/e2e/tasks.cy.js
@@ -0,0 +1,25 @@
+describe("POST /tasks", () => {
+  beforeEach(function () {
+    cy.fixture("tasks").then(function (tasks) {
+      this.tasks = tasks;
+    });
+  });
+
+  it("register a new task", function () {
+    const { user, task } = this.tasks.create;
+
+    cy.postSession(user).then((response) => {
+      cy.api({
+        url: "/tasks",
+        method: "POST",
+        body: task,
+        headers: {
+          authorization: response.body.token,
+        },
+        failOnStatusCode: false,
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+      });
+    });
+  });
+});
